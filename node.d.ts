7ed7b5e9f601dad28f31e80399947d9b28362e00
Lyumih <Lyumih@users.noreply.github.com>
2025-01-03 07:11:31 +0000

    Deploying to gh-pages from @ Lyumih/milis.snake@b44d522b19589bbc1cd206f6a4fbf5192ecb158e 🚀

--- node.d.ts
+++ node.d.ts
@@ -6458,7 +6458,7 @@ declare namespace $ {
 		uri( ): string
 		loading( ): string
 		decoding( ): string
-		cors( ): string
+		cors( ): any
 		natural_width( ): number
 		natural_height( ): number
 		load( next?: any ): any
--- web.js
+++ web.js
@@ -14396,7 +14396,7 @@ var $;
 			return "async";
 		}
 		cors(){
-			return "anonymous";
+			return null;
 		}
 		natural_width(){
 			return 0;
