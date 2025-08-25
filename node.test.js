6b246a0e04ba1c8b1401d9c2241b1eb40eaf29f3
Lyumih <Lyumih@users.noreply.github.com>
2025-08-25 07:14:44 +0000

    Deploying to gh-pages from @ Lyumih/milis.snake@b44d522b19589bbc1cd206f6a4fbf5192ecb158e 🚀

--- node.test.js
+++ node.test.js
@@ -10237,7 +10237,7 @@ var $;
 (function ($) {
     $.$hyoo_crus_vary_mapping = {
         nil: null,
-        bin: Uint8Array,
+        bin: (Uint8Array),
         bool: Boolean,
         int: BigInt,
         real: Number,
