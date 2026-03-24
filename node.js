09380acd55499dcd6804002653b9261450e1821d
Lyumih <Lyumih@users.noreply.github.com>
2026-03-24 07:50:27 +0000

    Deploying to gh-pages from @ Lyumih/milis.snake@b44d522b19589bbc1cd206f6a4fbf5192ecb158e 🚀

--- node.js
+++ node.js
@@ -6466,6 +6466,7 @@ var $;
 "use strict";
 var $;
 (function ($) {
+    let x = /x/[Symbol.matchAll];
     class $mol_regexp extends RegExp {
         groups;
         constructor(source, flags = 'gsu', groups = []) {
